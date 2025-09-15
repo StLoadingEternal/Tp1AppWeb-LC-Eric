abbad2452d39492a403d56ea6080d5e3bafa3d19
StLoadingEternal <2341056@cegeplimoilou.ca>
2025-09-15 13:01:22 -0400

    Issue #6 - Creation de la classe parent Uitilisateur
    Cette classe contient la structure de base des utilisateurs.

---
+++ tp1appweb-lc-eric/src/models/Utilisateur.js
@@ -0,0 +1,56 @@
+"use strict";
+
+export default class Utilisateur{
+    _id;
+    _nom;
+    _dateInscription;
+    _dateNaissance;
+    _role;
+
+
+    constructor(nom, dateInscription, dateNaissance, role, id) {
+        this._nom = nom;
+        this._dateInscription = dateInscription;
+        this._dateNaissance = dateNaissance;
+        this._role = role;
+        this._id = id;
+    }
+
+
+    get nom() {
+        return this._nom;
+    }
+
+    set nom(value) {
+        this._nom = value;
+    }
+
+    get dateInscription() {
+        return this._dateInscription;
+    }
+
+    set dateInscription(value) {
+        this._dateInscription = value;
+    }
+
+    get dateNaissance() {
+        return this._dateNaissance;
+    }
+
+    set dateNaissance(value) {
+        this._dateNaissance = value;
+    }
+
+    get role() {
+        return this._role;
+    }
+
+    set role(value) {
+        this._role = value;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+}

5fae8168dec4f0eec4635ae4cafa990e17222947
StLoadingEternal <2341056@cegeplimoilou.ca>
2025-09-15 13:24:43 -0400

    Issue #6 - creation d'un fichier json contenant 4 utilisateurs

---
+++ tp1appweb-lc-eric/src/scripts/utilisateurs.js
@@ -0,0 +1,33 @@
+
+
+let utilisateurs =  [
+    {
+        "_id": 1,
+        "_nom": "Admin User",
+        "_dateInscription": "2020-01-01",
+        "_dateNaissance": "1985-05-15",
+        "_role": "admin"
+    },
+    {
+        "_id": 2,
+        "_nom": "Alice Dupont",
+        "_dateInscription": "2022-07-23",
+        "_dateNaissance": "1990-02-10",
+        "_role": "journaliste"
+    },
+    {
+        "_id": 3,
+        "_nom": "Marc Lefevre",
+        "_dateInscription": "2021-03-12",
+        "_dateNaissance": "1988-11-22",
+        "_role": "journaliste"
+    },
+    {
+        "_id": 4,
+        "_nom": "Sophie Martin",
+        "_dateInscription": "2023-08-15",
+        "_dateNaissance": "1995-04-30",
+        "_role": "journaliste"
+    }
+];
+

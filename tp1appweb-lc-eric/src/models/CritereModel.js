67fb6d57b172a5c510c81107b604d5790ac9c4e9
StLoadingEternal <2341056@cegeplimoilou.ca>
2025-09-24 22:22:25 -0400

    Issue #34 Ajout d'un model pour les criteres

--- tp1appweb-lc-eric/src/models/CritereModel.js
+++ tp1appweb-lc-eric/src/models/CritereModel.js
@@ -1,3 +1,73 @@
 export default  class CritereModel{
+    _id
+    _titre
+    _noReference
+    _date
+    _categorie
+    _anneeNouvelle
+    _motsCles
 
+
+    constructor(id, noReference, titre, date, categorie, anneeNouvelle, motsCles) {
+        this._id = id;
+        this._titre = titre;
+        this._noReference = noReference;
+        this._date = date;
+        this._categorie = categorie;
+        this._anneeNouvelle = anneeNouvelle;
+        this._motsCles = motsCles;
+    }
+
+    get id(){
+        return this._id;
+    }
+
+    get noReference() {
+        return this._noReference;
+    }
+
+    set noReference(value) {
+        this._noReference = value;
+    }
+
+    get date() {
+        return this._date;
+    }
+
+    set date(value) {
+        this._date = value;
+    }
+
+    get categorie() {
+        return this._categorie;
+    }
+
+    set categorie(value) {
+        this._categorie = value;
+    }
+
+    get anneeNouvelle() {
+        return this._anneeNouvelle;
+    }
+
+    set anneeNouvelle(value) {
+        this._anneeNouvelle = value;
+    }
+
+    get motsCles() {
+        return this._motsCles;
+    }
+
+
+    get titre() {
+        return this._titre;
+    }
+
+    set titre(value) {
+        this._titre = value;
+    }
+
+    set motsCles(value) {
+        this._motsCles = value;
+    }
 }
